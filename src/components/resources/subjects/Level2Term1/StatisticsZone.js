f3d26ace7d827a9a2c1e97402b5418fcd0ecac79
Arnob Mahmud <arnob.tech.me@gmail.com>
2022-08-02 20:54:43 +0600

    section updated

--- src/components/resources/subjects/Level2Term1/StatisticsZone.js
+++ src/components/resources/subjects/Level2Term1/StatisticsZone.js
@@ -45,53 +45,6 @@ const StatisticsZone = () => {
 
   return (
     <>
-      <ResourceArea>
-        <div className="container">
-          <div className="row justify-content-center align-items-center">
-            {loading
-              ? Array(4)
-                  .fill()
-                  .map((_, index) => (
-                    <div className="col-lg-5 col-md-6 col-sm-12">
-                      <FolderPathCard
-                        title={
-                          <CustomSkeletonStructure2 height={30} width={`100`} />
-                        }
-                        subTitle={
-                          <CustomSkeletonStructure2 height={30} width={`100`} />
-                        }
-                        description={
-                          <CustomSkeletonStructure2 height={30} width={`100`} />
-                        }
-                        driveLink={
-                          <CustomSkeletonStructure2 height={40} width={`35%`} />
-                        }
-                      />
-                    </div>
-                  ))
-              : note.map((e, key) => (
-                  <div key={key} className="col-lg-5 col-md-6 col-sm-12">
-                    <div className="card">
-                      <div className="content">
-                        <h1>{e.title}</h1>
-                        <h5>{e.subTitle}</h5>
-                        <p>{e.description}</p>
-                        <div className="btn">
-                          <a
-                            href={e.driveLink}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            Visit Here <ImBook />
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-          </div>
-        </div>
-      </ResourceArea>
       <OnlineResourceArea>
         <div className="container">
           <div className="row justify-content-center">
@@ -193,6 +146,53 @@ const StatisticsZone = () => {
           </div>
         </div>
       </OnlineResourceArea>
+      <ResourceArea>
+        <div className="container">
+          <div className="row justify-content-center align-items-center">
+            {loading
+              ? Array(4)
+                  .fill()
+                  .map((_, index) => (
+                    <div className="col-lg-5 col-md-6 col-sm-12">
+                      <FolderPathCard
+                        title={
+                          <CustomSkeletonStructure2 height={30} width={`100`} />
+                        }
+                        subTitle={
+                          <CustomSkeletonStructure2 height={30} width={`100`} />
+                        }
+                        description={
+                          <CustomSkeletonStructure2 height={30} width={`100`} />
+                        }
+                        driveLink={
+                          <CustomSkeletonStructure2 height={40} width={`35%`} />
+                        }
+                      />
+                    </div>
+                  ))
+              : note.map((e, key) => (
+                  <div key={key} className="col-lg-5 col-md-6 col-sm-12">
+                    <div className="card">
+                      <div className="content">
+                        <h1>{e.title}</h1>
+                        <h5>{e.subTitle}</h5>
+                        <p>{e.description}</p>
+                        <div className="btn">
+                          <a
+                            href={e.driveLink}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            Visit Here <ImBook />
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+          </div>
+        </div>
+      </ResourceArea>
     </>
   );
 };
